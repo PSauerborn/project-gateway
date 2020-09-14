7176882988e759ab7fec8bf3c67d5411a99577a4
P24980 <P24980@UN990155>
2020-09-14 21:33:49 +0100

    changed indentation to space

--- api-gateway/helpers.go
+++ api-gateway/helpers.go
@@ -1,97 +1,97 @@
 package main
 
 import (
-	"fmt"
-	"strings"
-	"errors"
-	"net/http"
-	"net/url"
-	"github.com/dgrijalva/jwt-go"
-	log "github.com/sirupsen/logrus"
+    "fmt"
+    "strings"
+    "errors"
+    "net/http"
+    "net/url"
+    "github.com/dgrijalva/jwt-go"
+    log "github.com/sirupsen/logrus"
 )
 
 
 type JWTClaims struct {
-	Uid   string      `json:"uid"`
-	Admin bool	      `json:"admin"`
-	jwt.StandardClaims
+    Uid   string      `json:"uid"`
+    Admin bool	      `json:"admin"`
+    jwt.StandardClaims
 }
 
 // function used to parse JWT token
 func ParseJWToken(tokenString string) (*JWTClaims, error) {
-	log.Info(fmt.Sprintf("parsing JWToken %s", tokenString))
-	// parse token using JWT secret
-	token, err := jwt.ParseWithClaims(tokenString, &JWTClaims{}, func(token *jwt.Token) (interface{}, error) {
-		return []byte(JWTSecret), nil
-	})
-	// parse token into custom claims object
-	if customClaims, ok := token.Claims.(*JWTClaims); ok && token.Valid {
-		return customClaims, nil
-	} else {
-		log.Error(fmt.Errorf("unable to parse JWT claims: %v", err))
-		return nil, errors.New("invalid JWToken")
-	}
+    log.Info(fmt.Sprintf("parsing JWToken %s", tokenString))
+    // parse token using JWT secret
+    token, err := jwt.ParseWithClaims(tokenString, &JWTClaims{}, func(token *jwt.Token) (interface{}, error) {
+        return []byte(JWTSecret), nil
+    })
+    // parse token into custom claims object
+    if customClaims, ok := token.Claims.(*JWTClaims); ok && token.Valid {
+        return customClaims, nil
+    } else {
+        log.Error(fmt.Errorf("unable to parse JWT claims: %v", err))
+        return nil, errors.New("invalid JWToken")
+    }
 }
 
 // define function used to extract token from request header
 func ExtractToken(header string) (string, error) {
-	if strings.HasPrefix(header, "Bearer ") {
-		return header[7:], nil
-	}
-	return "", errors.New(fmt.Sprintf("received invalid token schema: %s", header))
+    if strings.HasPrefix(header, "Bearer ") {
+        return header[7:], nil
+    }
+    return "", errors.New(fmt.Sprintf("received invalid token schema: %s", header))
 }
 
 // define function used to authenticate user
 func AuthenticateUser(request *http.Request) (*JWTClaims, error) {
-	// extract token from authentication header
-	tokenString, err := ExtractToken(request.Header.Get("Authorization"))
-	if err != nil || tokenString == "undefined" {
-		log.Error(fmt.Errorf("invalid authorization header: %v", err))
-		return nil, errors.New("received invalid authorization header")
-	}
-	// parse token claims using token string
-	tokenClaims, err := ParseJWToken(tokenString)
-	if err != nil {
-		log.Error(fmt.Errorf("unable to parse JWToken: %v", err))
-		return nil, err
-	}
-	return tokenClaims, nil
+    // extract token from authentication header
+    tokenString, err := ExtractToken(request.Header.Get("Authorization"))
+    if err != nil || tokenString == "undefined" {
+        log.Error(fmt.Errorf("invalid authorization header: %v", err))
+        return nil, errors.New("received invalid authorization header")
+    }
+    // parse token claims using token string
+    tokenClaims, err := ParseJWToken(tokenString)
+    if err != nil {
+        log.Error(fmt.Errorf("unable to parse JWToken: %v", err))
+        return nil, err
+    }
+    return tokenClaims, nil
 }
 
 // define function used to determine relevant proxy URL
 func getProxyURI(path string) (ApplicationDetails, error) {
-	// pass request path and extract application name
-	params := strings.Split(path, "/")
-	if len(params) < 2 {
-		log.Error("received request to empty route")
-		return ApplicationDetails{}, errors.New("invalid request path")
-	}
-	log.Info(fmt.Sprintf("getting redirect for application '%s'", params[1]))
+    // pass request path and extract application name
+    params := strings.Split(path, "/")
+    if len(params) < 2 {
+        log.Error("received request to empty route")
+        return ApplicationDetails{}, errors.New("invalid request path")
+    }
+    log.Info(fmt.Sprintf("getting redirect for application '%s'", params[1]))
 
-	// get application details from postgres server
-	details, err := persistence.GetModuleDetails(params[1])
-	if err != nil {
-		return ApplicationDetails{}, err
-	}
-	return details, nil
+    // get application details from postgres server
+    details, err := persistence.GetModuleDetails(params[1])
+    if err != nil {
+        return ApplicationDetails{}, err
+    }
+    return details, nil
 }
 
 // function used to set proxy headers headers on request
 func SetProxyHeaders(request *http.Request, url *url.URL) {
-	request.URL.Host = url.Host
-	request.URL.Scheme = url.Scheme
-	request.Header.Set("X-Forwarded-Host", request.Header.Get("Host"))
-	request.Host = url.Host
+    request.URL.Host = url.Host
+    request.URL.Scheme = url.Scheme
+    request.Header.Set("X-Forwarded-Host", request.Header.Get("Host"))
+    request.Host = url.Host
 }
 
 func SetCorsHeaders(response http.ResponseWriter, request *http.Request) {
-	origin := request.Header.Get("Origin")
-	if len(origin) > 0 {
-		response.Header().Set("Access-Control-Allow-Origin", origin)
-	} else {
-		response.Header().Set("Access-Control-Allow-Origin", "*")
-	}
-	response.Header().Set("Access-Control-Allow-Credentials", "true")
-	response.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-	response.Header().Set("Access-Control-Allow-Methods", "POST,OPTIONS,GET,PUT,PATCH,DELETE")
+    origin := request.Header.Get("Origin")
+    if len(origin) > 0 {
+        response.Header().Set("Access-Control-Allow-Origin", origin)
+    } else {
+        response.Header().Set("Access-Control-Allow-Origin", "*")
+    }
+    response.Header().Set("Access-Control-Allow-Credentials", "true")
+    response.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+    response.Header().Set("Access-Control-Allow-Methods", "POST,OPTIONS,GET,PUT,PATCH,DELETE")
 }
--- api-gateway/persistence.go
+++ api-gateway/persistence.go
@@ -1,51 +1,51 @@
 package main
 
 import (
-	"fmt"
-	"time"
-	"context"
-	"github.com/jackc/pgx/v4/pgxpool"
-	"github.com/google/uuid"
-	log "github.com/sirupsen/logrus"
+    "fmt"
+    "time"
+    "context"
+    "github.com/jackc/pgx/v4/pgxpool"
+    "github.com/google/uuid"
+    log "github.com/sirupsen/logrus"
 )
 
 var persistence *Persistence
 
 type Persistence struct {
-	conn *pgxpool.Pool
+    conn *pgxpool.Pool
 }
 
 // function used to connect postgres connection
 func ConnectPersistence() {
-	log.Info(fmt.Sprintf("attempting postgres connection with connection string %s", PostgresConnection))
-	db, err := pgxpool.Connect(context.Background(), PostgresConnection)
-	if err != nil {
-		log.Fatal(fmt.Errorf("unable to connect to postgres server: %v", err))
-	}
-	log.Info("successfully connected to postgres")
-	// connect persistence and assign to persistence var
-	persistence = &Persistence{ db }
+    log.Info(fmt.Sprintf("attempting postgres connection with connection string %s", PostgresConnection))
+    db, err := pgxpool.Connect(context.Background(), PostgresConnection)
+    if err != nil {
+        log.Fatal(fmt.Errorf("unable to connect to postgres server: %v", err))
+    }
+    log.Info("successfully connected to postgres")
+    // connect persistence and assign to persistence var
+    persistence = &Persistence{ db }
 }
 
 func(db Persistence) GetModuleDetails(application string) (ApplicationDetails, error) {
 
-	log.Debug(fmt.Sprintf("fetching module details for module %s", application))
-	var (applicationId uuid.UUID; description, redirectUrl string; created time.Time; trim bool)
+    log.Debug(fmt.Sprintf("fetching module details for module %s", application))
+    var (applicationId uuid.UUID; description, redirectUrl string; created time.Time; trim bool)
 
-	// get module details from postgres server
-	results := db.conn.QueryRow(context.Background(), "SELECT application_id,created_at,description,redirect_url,trim_app_name FROM applications WHERE application_name=$1", application)
-	err := results.Scan(&applicationId, &created, &description, &redirectUrl, &trim)
-	if err != nil {
-		log.Error(fmt.Errorf("unable to retrieve module details: %v", err))
-		return ApplicationDetails{}, err
-	}
-	details := ApplicationDetails{
-		ApplicationID: applicationId,
-		ApplicationName: application,
-		CreatedAt: created,
-		Description: description,
-		RedirectURL: redirectUrl,
-		TrimAppName: trim,
-	}
-	return details, nil
+    // get module details from postgres server
+    results := db.conn.QueryRow(context.Background(), "SELECT application_id,created_at,description,redirect_url,trim_app_name FROM applications WHERE application_name=$1", application)
+    err := results.Scan(&applicationId, &created, &description, &redirectUrl, &trim)
+    if err != nil {
+        log.Error(fmt.Errorf("unable to retrieve module details: %v", err))
+        return ApplicationDetails{}, err
+    }
+    details := ApplicationDetails{
+        ApplicationID: applicationId,
+        ApplicationName: application,
+        CreatedAt: created,
+        Description: description,
+        RedirectURL: redirectUrl,
+        TrimAppName: trim,
+    }
+    return details, nil
 }
